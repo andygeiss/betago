37bc8dc4d68a0d0e5c1f4c6d1d2b59d7e3a50be3
andygeiss <4ndygeiss@gmail.com>
2017-11-30 08:46:23 +0100

    protocol added

--- business/protocol/protocol_test.go
+++ business/protocol/protocol_test.go
@@ -1,1 +1,49 @@
 package protocol_test
+
+import (
+	"testing"
+
+	"github.com/andygeiss/miabot/business/protocol"
+)
+
+func TestIfAnnounceIsValid(t *testing.T) {
+	commands := make(chan string)
+	go protocol.Announce("4,1", "TOKEN", commands)
+	select {
+	case command := <-commands:
+		if command != "ANNOUNCE;4,1;TOKEN" {
+			t.Errorf("Announce failed!")
+		}
+	}
+}
+func TestIfJoinIsValid(t *testing.T) {
+	commands := make(chan string)
+	go protocol.Join("TOKEN", commands)
+	select {
+	case command := <-commands:
+		if command != "JOIN;TOKEN" {
+			t.Errorf("Join failed!")
+		}
+	}
+}
+func TestIfRollIsValid(t *testing.T) {
+	commands := make(chan string)
+	go protocol.Roll("TOKEN", commands)
+	select {
+	case command := <-commands:
+		if command != "ROLL;TOKEN" {
+			t.Errorf("Roll failed!")
+		}
+	}
+}
+
+func TestIfSeeIsValid(t *testing.T) {
+	commands := make(chan string)
+	go protocol.See("TOKEN", commands)
+	select {
+	case command := <-commands:
+		if command != "SEE;TOKEN" {
+			t.Errorf("See failed!")
+		}
+	}
+}
